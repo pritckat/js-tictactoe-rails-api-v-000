5ef35b16d737522766e48c0052ee2446db9dc089
Katrina Pritchard <pritckat@gmail.com>
2018-10-25 18:23:12 +0000

    automatically backed up by learn

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,24 @@
 // Code your JavaScript / jQuery solution here
+
+
+
+let turnNumber = 0;
+let gameNumber = 0;
+
+$(document).ready(function() {
+  attachListeners();
+});
+
+let player = () => turnNumber % 2 ? 'O' : 'X';
+
+function turn(cell) {
+  let marker = player();
+  $(cell).text(marker);
+  turnNumber++;
+}
+
+function attachListeners() {
+  $('td').on('click', function() {
+    turn(this);
+  })
+}
