c12856119281de57496207e3c3d1a141df2531f8
Katrina Pritchard <pritckat@gmail.com>
2018-10-25 19:39:56 +0000

    automatically backed up by learn

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -25,7 +25,7 @@ function checkWinner() {
   var board = {};
   var winner = false;
   $('td').text((index, square) => board[index] = square);
-  
+
   WIN_COMBOS.some(function(combo) {
     if (board[combo[0]] !== "" && board[combo[0]] === board[combo[1]] && board[combo[0]] === board[combo[2]]) {
       setMessage(`Player ${board[combo[0]]} Won!`)
