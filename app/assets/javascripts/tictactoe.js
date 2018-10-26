024f5fe91a54629f64671a7258051672a867f3a2
Katrina Pritchard <pritckat@gmail.com>
2018-10-26 00:06:00 +0000

    automatically backed up by learn

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -78,6 +78,7 @@ function previousGames() {
 
 function makeButton(game) {
   $('#games').append(`<button id="gameid-${game.id}">${game.id}</button><br>`)
+  $(`#gameid-${game.id}`).on('click', () => reloadGame(game.id));
 }
 
 function saveGame() {
@@ -101,4 +102,34 @@ function saveGame() {
       makeButton(currentGame)
     });
   }
+}
+
+function reloadGame(gameID) {
+  document.getElementById('message').innerHTML = '';
+
+  const xhr = new XMLHttpRequest;
+  xhr.overrideMimeType('application/json');
+  xhr.open('GET', `/games/${gameID}`, true);
+  xhr.onload = () => {
+    const data = JSON.parse(xhr.responseText).data;
+    const id = data.id;
+    const state = data.attributes.state;
+
+    let index = 0;
+    for (let y = 0; y < 3; y++) {
+      for (let x = 0; x < 3; x++) {
+        document.querySelector(`[data-x="${x}"][data-y="${y}"]`).innerHTML = state[index];
+        index++;
+      }
+    }
+
+    turn = state.join('').length;
+    currentGame = id;
+
+    if (!checkWinner() && turn === 9) {
+      setMessage('Tie game.');
+    }
+  };
+
+  xhr.send(null);
 }
